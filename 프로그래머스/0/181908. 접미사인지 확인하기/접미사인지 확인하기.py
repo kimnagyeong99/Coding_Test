fe077003798da3a62bc8878be5261488db754d47
kimnagyeong99 <knk990928@gmail.com>
2024-08-08 23:22:46 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -0,0 +1,8 @@
+def solution(my_string, is_suffix):
+    if len(my_string) < len(is_suffix):
+        return 0
+    else:
+        if my_string[len(my_string)-len(is_suffix):] == is_suffix:
+            return 1
+        else:
+            return 0
