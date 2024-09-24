65b361734b78487751550c0f0e5040ee03198f11
kimnagyeong99 <knk990928@gmail.com>
2024-09-25 08:35:23 +0900

    [level 2] Title: 올바른 괄호, Time: 9.14 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,15 @@
+def solution(s):
+    answer = []
+    for i in s:
+        if i == "(":
+            answer.append(i)
+        else:
+            if len(answer) == 0:
+                return False
+            else:
+                answer.pop()
+    
+    if len(answer) == 0:
+        return True
+    else:
+        return False
