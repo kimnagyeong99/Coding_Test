f39cef7ca8a8cb14e1a0bc478c042796f141b58d
kimnagyeong99 <knk990928@gmail.com>
2025-10-30 16:39:40 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.90 ms, Memory: 59 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Solution {
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    
+    public int solution(int[][] maps) {
+        
+        int n = maps.length;
+        int m = maps[0].length;
+        
+        boolean[][] visited = new boolean[n][m];
+        int[][] dist = new int[n][m];
+        
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0,0});
+        visited[0][0] = true;
+        dist[0][0] = 1;
+        
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int x = cur[0], y = cur[1];
+            
+            if (x == n-1 && y == m-1){
+                return dist[x][y];
+            }
+            
+            for(int i = 0; i < 4; i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                
+                if (0 <= nx && nx < n && 0<= ny && ny < m && maps[nx][ny] == 1 && !visited[nx][ny]){
+                    visited[nx][ny] = true;
+                    dist[nx][ny] = dist[x][y] +1;
+                    q.add(new int[]{nx, ny});
+                    
+                }
+            }
+        }
+    return -1;
+    }
+}
